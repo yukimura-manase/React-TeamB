da79f90d4f1d6cb47e41a1dd8cc000aa6f8f5d13
yukimura-manase <masahiro19951212@gmail.com>
2021-09-01 15:50:53 +0900

    初期設定完了

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { useDispatch,useSelector } from 'react-redux'; // 仮置きサンプル
+import {useHistory} from "react-router-dom"
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom' // Router設定仮置き
+
+
+const App = ()=> {
+  return (
+    <React.Fragment>
+      <h1>TeamBの制作物</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/reducers/StoreState.js
@@ -0,0 +1,4 @@
+// Stateを管理するStore => Actionsを受け取ったStateの変更処理をする部門を内部に持っている。
+// StateとStateの更新処理 => ファイル単位のuseStateのようなもの。
+
+
