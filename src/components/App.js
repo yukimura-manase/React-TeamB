0fa4c4d95a85f0b1b22c79d5503103bb12bc5d1c
rksYuta <kobayashi.yuta.rks@gmail.com>
2021-09-02 15:26:49 +0900

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import { useDispatch,useSelector } from 'react-redux'; // 仮置きサンプル
-import {useHistory} from "react-router-dom"
+import {useHistory} from "react-router-dom";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from 'react-router-dom' // Router設定仮置き
+// import Product from './Product'
 
 import  '../service/firebase'
 import Header from './Header'
+import {Detail} from './detail'
+import {Product} from './Product'
+
 
 const App = ()=> {
   return (
     <React.Fragment>
+      <Router>
       <h1>TeamBの制作物</h1>
       <Header/>
+      <Switch>
+        <Route path='/detail/:id' component={Detail}></Route>
+        <Route path='/' component={Product}></Route>
+      </Switch>
+      </Router>
     </React.Fragment>
   );
 }
--- src/reducers/StoreState.js
+++ src/reducers/StoreState.js
@@ -1,8 +1,22 @@
 // Stateを管理するStore => Actionsを受け取ったStateの変更処理をする部門を内部に持っている。
 // StateとStateの更新処理 => ファイル単位のuseStateのようなもの。
 
-const StoreState=()=>{
-  return 100
+// const StoreState=()=>{
+//   return 100
+// }
+
+// export default StoreState;
+
+export const initialState ={
+  Curry:[{id:1,img:'カレーの画像',name:'餃子定食',lsizePrice:500,msizePrice:1000},
+  {id:2,img:'カレーうどんの画像',name:'チャーハン',lsizePrice:600,msizePrice:1200},
+  {id:3,img:'カレーそばの画像',name:'青椒肉絲',lsizePrice:300,msizePrice:1000},]
 }
 
-export default StoreState;
+// eslint-disable-next-line import/no-anonymous-default-export
+export default(state=initialState,action) =>{
+  switch(action.type){
+    default:
+      return state
+  }
+}
